6d5519f4600a7f0ddc6de48af47bb3c11b4f8af9
AndradeTC86 <andrade-tc@uol.com.br>
2025-01-29 19:46:42 -0300

    Adicionando testes

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -1,10 +1,15 @@
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.windows.WindowsDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class Calculator {
 
@@ -39,25 +44,52 @@ public class Calculator {
     @BeforeMethod
     public void clear(){
         System.out.println("Clearing the Calculator");
+        calcsession.findElement(AppiumBy.accessibilityId("clearButton")).click();
     }
 
     @Test
     public void addition(){
         System.out.println("Running Addition Operation");
+        calcsession.findElement(AppiumBy.accessibilityId("num2Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("plusButton")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("num7Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("equalButton")).click();
+        Assert.assertEquals(GetDisplayResult(), "9");
     }
 
     @Test
     public void multiplication(){
         System.out.println("Running Multiplication Operation");
+        calcsession.findElement(AppiumBy.accessibilityId("num6Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("multiplyButton")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("num9Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("equalButton")).click();
+        Assert.assertEquals(GetDisplayResult(), "54");
     }
 
     @Test
     public void division(){
         System.out.println("Running Division Operation");
+        calcsession.findElement(AppiumBy.accessibilityId("num8Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("divideButton")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("num2Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("equalButton")).click();
+        Assert.assertEquals(GetDisplayResult(), "4");
     }
 
     @Test
     public void subtraction(){
         System.out.println("Running Subtraction Operation");
+        calcsession.findElement(AppiumBy.accessibilityId("num9Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("minusButton")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("num3Button")).click();
+        calcsession.findElement(AppiumBy.accessibilityId("equalButton")).click();
+        Assert.assertEquals(GetDisplayResult(), "6");
+    }
+
+    public String GetDisplayResult(){
+        WebDriverWait wait = new WebDriverWait(calcsession, Duration.ofSeconds(10));
+        calcresult = wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.accessibilityId("CalculatorResults")));
+        return calcresult.getText().replace("A exibição é", "").trim();
     }
 }
